9b808a71ed7fc9736c2dfab3e87ac7746e9f6f35
Crazy-Alien230 <69044724+Crazy-Alien230@users.noreply.github.com>
2020-08-03 15:50:31 +0530

    Errorss

--- php/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $conn= new mysqli("localhost","root","","farmersite");
-  // $conn= new mysqli("localhost","parvinder","pzxuTAmsgGR3","test_finstreet_courses");
-  if($conn->connect_error){
-      die("connection Failed" .$conn->connect_error);
-  }
-
-
-?>
